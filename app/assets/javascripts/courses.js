8b555d189bd17a562ed6d138c94a37c5cd3439b1
ash-h <a.henry@kuumba.co.uk>
2020-08-24 08:16:57 +0100

    add content and filtering for sprint 3

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -1,41 +1,158 @@
 const data = [
     {
-        "Title": "Learn to Code for Data Analysis",
-        "Desc": "Learn how to write your own computer programs, one line of code at a time. Find out how to access open data, clean and analyse it, and produce visualisations, and how to present your analyses, privately or publicly. You'll be coding in Python, a widely used programming language across all disciplines.",
-        "Provider": "The Open University",
-        "Level": "Advanced",
-        "Duration": "1d",
-        "": "",
-        "__1": ""
+        "Title": "Learn My Way",
+        "Desc": "Collection of resources to help you use the internet. Learn how to use your computer or device, find a job online, use public service and use office programs like Microsoft Word.",
+        "Provider": "Good Things Foundation",
+        "Duration": "0 to 4 hours",
+        "Subject": "Computer essentials",
+        "Certificate of completion": "no",
+        "Study type": "self-paced"
     },
     {
-        "Title": "Create a Professional Online Presence",
-        "Desc": "Explore how to present yourself professionally online. Find out more about different social media platforms and how the way that you conduct yourself online can impact your professional reputation. Learn about safety and privacy and the ways that your online activity might be viewed by different audiences.",
-        "Provider": "FutureLearn",
-        "Level": "Intermediate",
-        "Duration": "4 hours in total over 2 weeks",
-        "": "",
-        "__1": ""
+        "Title": "Make It Click",
+        "Desc": "Over 150 resources to help with everything from sending emails and creating a CV to editing photos and advice for working from home.",
+        "Provider": "Good Things Foundation",
+        "Duration": "0 to 4 hours",
+        "Subject": "Computer essentials",
+        "Certificate of completion": "no",
+        "Study type": "self-paced"
+    },
+    {
+        "Title": "Get the best out of Microsoft Outlook",
+        "Desc": "Learn how to send an email, book a meeting, manage your calendar and organise a to-do list.",
+        "Provider": "Microsoft",
+        "Duration": "0 to 4 hours",
+        "Subject": "Computer essentials",
+        "Certificate of completion": "no",
+        "Study type": "self-paced"
+    },
+    {
+        "Title": "Learn For Everyday Life",
+        "Desc": "Inspire your digital journey with lessons on a variety of skills to help you master video calling, deal with online fraud, improve your CV, present yourself and work with others safely online.",
+        "Provider": "Lloyds Bank",
+        "Duration": "0 to 4 hours",
+        "Subject": "Computer essentials",
+        "Certificate of completion": "no",
+        "Study type": "self-paced"
+    },
+    {
+        "Title": "Everyday Maths",
+        "Desc": "Refresh your maths skills to help with everyday life or get on in your job. From using a calculator, learning to convert fractions to decimals and understanding percentages, to measuring objects, distances and weights. Understand how to use tables, diagrams and charts. You'll get lots of practice using the maths you need in daily life.",
+        "Provider": "The Open University",
+        "Duration": "48 hours",
+        "Subject": "Practical maths",
+        "Certificate of completion": "Yes",
+        "Study type": "self-paced"
     },
     {
         "Title": "Introduction to Bookkeeping",
         "Desc": "Get an introduction to the essential skills and concepts of bookkeeping and accounting. The course covers double-entry bookkeeping, how to prepare a trial balance and the two principal financial statements: the balance sheet and the profit and loss account.",
         "Provider": "The Open University",
-        "Level": "Introductory",
-        "Duration": "8h",
-        "": "",
-        "__1": ""
+        "Duration": "8 hours",
+        "Subject": "Practical maths \nBusiness and marketing",
+        "Certificate of completion": "Yes",
+        "Study type": "self-paced"
     },
     {
-        "Title": "Learn My Way",
-        "Desc": " Resources to help you get started online. Find topics from getting to grips with your computer, tablet or mobile phone to how to use office programs like Excel, Word and PowerPoint. You can also learn to manage your money, use public services or find jobs and careers advice online",
-        "Provider": "Good Things Foundation",
-        "Level": "Introductory",
-        "Duration": "Bite-sized resources from a few minutes to a few hours",
-        "": "",
-        "__1": ""
+        "Title": "Introduction to Finance",
+        "Desc": "Understand the essential analytical tools and financial reports that are used to manage organisations, like understanding balance sheets, profit and loss accounts and cash flow forecast and managing budgets.",
+        "Provider": "Corndel",
+        "Duration": "40 hours",
+        "Subject": "Practical maths \nBusiness and marketing",
+        "Certificate of completion": "No",
+        "Study type": "self-paced"
+    },
+    {
+        "Title": "Create a Professional Online Presence",
+        "Desc": "Explore how to present yourself professionally online. Find out more about different social media platforms and how the way that you conduct yourself online can impact your professional reputation. Learn about safety and privacy and the ways that your online activity might be viewed by different audiences.",
+        "Provider": "FutureLearn",
+        "Duration": "4 hours in total over 2 weeks",
+        "Subject": "Workplace essentials",
+        "Certificate of completion": "Yes",
+        "Study type": "set start dates"
+    },
+    {
+        "Title": "Thriving in the Digital Workplace",
+        "Desc": "Boost your confidence and build the skills you need to use digital technology in the workplace. Explore jobs that need digital skills through real life case studies. Get to know what digital skills you're most likely to need in the future to stay one step ahead and develop techniques to enhance wellbeing and resilience at work.",
+        "Provider": "FutureLearn",
+        "Duration": "12 hours",
+        "Subject": "Workplace essentials",
+        "Certificate of completion": "Yes",
+        "Study type": "self-paced"
+    },
+    {
+        "Title": "How to Create Great Online Content",
+        "Desc": "Find out about different types of online content and what makes them effective in different situations. Discover the importance of tone, style and format in your communications, and develop best practice for communicating effectively online",
+        "Provider": "FutureLearn",
+        "Duration": "4 hours",
+        "Subject": "Business and marketing",
+        "Certificate of completion": "Yes",
+        "Study type": "self-paced"
+    },
+    {
+        "Title": "Presenting Your Work with Impact",
+        "Desc": "Learn to create and present engaging content clearly and confidently in a range of situations, like in interviews and business meetings. Explore different types of presentation software and learn techniques to convey your ideas with confidence.",
+        "Provider": "FutureLearn",
+        "Duration": "4 hours",
+        "Subject": "Workplace essentials",
+        "Certificate of completion": "Yes",
+        "Study type": "self-paced"
+    },
+    {
+        "Title": "Digital Skills: Social Media (for business)",
+        "Desc": "A course for those wanting to use social media effectively to develop their entrepreneurial skills, to enter the workplace or to change career. Learn how to use social media to establish and improve your brand, set campaign objectives and identify your target audience. Create engaging content and discover the best places to pitch it.",
+        "Provider": "FutureLearn",
+        "Duration": "2 hours",
+        "Subject": "Business and marketing",
+        "Certificate of completion": "Yes",
+        "Study type": "self-paced"
+    },
+    {
+        "Title": "Fundamentals of Digital Marketing",
+        "Desc": "Learn how to market goods or services online through a series of short video tutorials and knowledge quizzes. The course covers topics like creating a website and getting it known, marketing your business locally and internationally and advertising using social media. There are modules covering how to sell online as well as how to analyse the success of your marketing plan.",
+        "Provider": "Google Digital Garage",
+        "Duration": "40 hours",
+        "Subject": "Business and marketing",
+        "Certificate of completion": "Yes",
+        "Study type": "self-paced"
+    },
+    {
+        "Title": "Discovering Computer Networks: hands on in the Open Networking Lab",
+        "Desc": "Learn how computer networks operate, how data is sent around the internet and put your understanding into practice using an inbuilt network simulator to create your own simulated computer networks.",
+        "Provider": "The Open University",
+        "Duration": "24 hours",
+        "Subject": "Coding and computer science",
+        "Certificate of completion": "Yes",
+        "Study type": "self-paced"
+    },
+    {
+        "Title": "Learn to Code for Data Analysis",
+        "Desc": "Learn how to write your own computer programs, one line of code at a time. Find out how to access open data, clean and analyse it, and produce visualisations, and how to present your analyses, privately or publicly. You'll be coding in Python, a widely used programming language across all disciplines.",
+        "Provider": "The Open University",
+        "Duration": "1d",
+        "Subject": "Coding and computer science",
+        "Certificate of completion": "Yes",
+        "Study type": "self-paced"
+    },
+    {
+        "Title": "Programming Essentials in Python",
+        "Desc": "Learn how to code in Python: an object-oriented programming language used by small companies to tech giants. Gain skills to write your own computer programmes, or to design a web or desktop application. You don't need to have any previous programming experience or knowledge to do this course.",
+        "Provider": "Cisco Networking Academy",
+        "Duration": "70 hours",
+        "Subject": "Coding and computer science",
+        "Certificate of completion": "No",
+        "Study type": "self-paced"
+    },
+    {
+        "Title": "Introduction to Cyber Security: Stay Safe Online",
+        "Desc": "Learn how to stay safe online at home and at work. The course will help you to recognise cyber threats and the steps to take to reduce your risks online. Find out about malware, viruses, trojans, network security, cryptography, identity theft and risk management.",
+        "Provider": "The Open University",
+        "Duration": "24 hours",
+        "Subject": "Coding and computer science",
+        "Certificate of completion": "Yes",
+        "Study type": "self-paced"
     }
-];
+]
 
 const courseList = document.getElementsByClassName('course-list')[0];
 
@@ -54,19 +171,48 @@ filterCourses = (level, category) => {
     clearCourses();
     loadCourses(result);
 }
+
+filterCategory = (category) => {
+    const result = data.filter(course => course.Subject.includes(category));
+
+    //clearCourses();
+    loadCourses(result);
+}
 const filterButton = document.getElementsByClassName('filter-button')[0];
 const resetButton = document.getElementsByClassName('reset-button')[0];
+const categoryFilterButton = document.getElementsByClassName('category-filter-button')[0];
 
 resetButton.addEventListener('click', (e)=>{
     e.preventDefault();
     clearCourses();
     loadCourses(data);
 })
-filterButton.addEventListener('click', (e)=>{
+if(filterButton) {
+    filterButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        const selectedItem = document.querySelector('.govuk-radios__input:checked');
+        const level = selectedItem.value;
+        filterCourses(level);
+    })
+}
+
+categoryFilterButton.addEventListener('click', (e)=>{
     e.preventDefault();
-    const selectedItem = document.querySelector('.govuk-radios__input:checked');
-    const level = selectedItem.value;
-    filterCourses(level);
+    const selectedValues = [];
+    const selectedItem = document.querySelectorAll('.govuk-checkboxes__input:checked');
+    for (let x of selectedItem) {
+        selectedValues.push(x.value);
+    }
+    clearCourses();
+    for (let x of selectedValues) {
+        filterCategory(x);
+    }
+
+    console.log(selectedValues);
+
+    // const selectedItem = document.querySelector('.govuk-radios__input:checked');
+    // const level = selectedItem.value;
+    // filterCourses(level);
 })
 
 
@@ -79,7 +225,6 @@ printCoursesOnScreen = (title, desc, provider, level, duration) => {
             <div class="govuk-!-margin-bottom-1"><span class="govuk-hint">${provider}</span></div>
             <div class="govuk-!-margin-bottom-1"><h2 class="govuk-heading-m"><a href class="govuk-link">${title}</a></h2></div>
             <div class="govuk-!-margin-bottom-1"><span class="govuk-!-font-weight-bold">Description : </span>${desc}</div>
-            <div class="govuk-!-margin-bottom-1"><span class="govuk-!-font-weight-bold">Level : </span>${level}</div>
             <div class="govuk-!-margin-bottom-1"><span class="govuk-!-font-weight-bold">Study time : </span>${duration}</div>
             </li>
             <hr class=" govuk-section-break govuk-section-break--visible govuk-section-break--l">`
